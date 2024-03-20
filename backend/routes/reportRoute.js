3d4e213971f1001e40ef42b10772c9ba6032525a
Rahulsingh1939 <rahulsingh19390@gmail.com>
2024-03-21 01:04:50 +0530

    Report Route Completed

---
+++ backend/routes/reportRoute.js
@@ -0,0 +1,32 @@
+const authMiddleware = require("../middlewares/authMiddleware");
+
+const express = require("express");
+const {
+  addReportController,
+  getAllReportController,
+  getUserReportController,
+} = require("../controller/reportController");
+const isAdminMiddleware = require("../middlewares/isAdminMiddleware");
+
+const router = express.Router();
+
+router.post(
+  "/add-report",
+  authMiddleware,
+  isAdminMiddleware,
+  addReportController
+);
+router.post(
+  "/get-all-reports",
+  authMiddleware,
+  isAdminMiddleware,
+  getAllReportController
+);
+router.post(
+  "/get-all-reports-by-user",
+  authMiddleware,
+  isAdminMiddleware,
+  getUserReportController
+);
+
+module.exports = router;
