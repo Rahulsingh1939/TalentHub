5912593836f8ed5a81f72dc2cf1f8c3a85aa12c8
Rahulsingh1939 <rahulsingh19390@gmail.com>
2024-03-20 23:18:42 +0530

    Working on Exam Route

--- backend/routes/examRoute.js
+++ backend/routes/examRoute.js
@@ -10,20 +10,52 @@ const {
   editQuestionController,
 } = require("../controller/examController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const isAdminMiddleware = require("../middlewares/isAdminMiddleware");
 
 const router = express.Router();
 
-router.post("/add", authMiddleware, addExamController);
-router.post("/get-all-exams", authMiddleware, getAllExamController);
-router.post("/get-exam-by-id", authMiddleware, getExamByIdController);
-router.post("/edit-exam-by-id", authMiddleware, editExamByIdController);
-router.post("/delete-exam-by-id", authMiddleware, deleteExamByIdController);
-router.post("/add-question-to-exam", authMiddleware, addQuestionController);
+router.post("/add", authMiddleware, isAdminMiddleware, addExamController);
+router.post(
+  "/get-all-exams",
+  authMiddleware,
+  isAdminMiddleware,
+  getAllExamController
+);
+router.post(
+  "/get-exam-by-id",
+  authMiddleware,
+  isAdminMiddleware,
+  getExamByIdController
+);
+router.post(
+  "/edit-exam-by-id",
+  authMiddleware,
+  isAdminMiddleware,
+  editExamByIdController
+);
+router.post(
+  "/delete-exam-by-id",
+  authMiddleware,
+  isAdminMiddleware,
+  deleteExamByIdController
+);
+router.post(
+  "/add-question-to-exam",
+  authMiddleware,
+  isAdminMiddleware,
+  addQuestionController
+);
 router.post(
   "/delete-question-in-exam",
   authMiddleware,
+  isAdminMiddleware,
   deleteQuestionController
 );
-router.post("/edit-question-in-exam", authMiddleware, editQuestionController);
+router.post(
+  "/edit-question-in-exam",
+  authMiddleware,
+  isAdminMiddleware,
+  editQuestionController
+);
 
 module.exports = router;
