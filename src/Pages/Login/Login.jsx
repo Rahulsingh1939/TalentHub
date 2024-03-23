83e4bbb664ef91c663307433cf6640364858e6a8
Rahulsingh1939 <rahulsingh19390@gmail.com>
2024-03-23 14:53:37 +0530

    Issue unresolved Glogin

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../context/auth";
 import toast from "react-hot-toast";
+import { GoogleLogin } from "@react-oauth/google";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -34,6 +35,14 @@ const Login = () => {
       console.log(error);
       toast.error("Something went wrong");
     }
+  };
+
+  const responseMessage = (response) => {
+    // console.log(response);
+    toast.error('Sorry, Please Login Manually')
+  };
+  const errorMessage = (error) => {
+    console.log(error);
   };
   return (
     <>
@@ -114,28 +123,13 @@ const Login = () => {
                   </span>
                   <span className="h-px bg-gray-400 w-14" />
                 </span>
-                <div className="flex flex-col space-y-4">
-                  <a
-                    href="#"
-                    className="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-gray-800 rounded-md group hover:bg-gray-800 focus:outline-none"
-                  >
-                    <span>
-                      <svg
-                        className="w-5 h-5 text-gray-800 fill-current group-hover:text-white"
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-                        />
-                      </svg>
-                    </span>
-                    <span className="text-sm font-medium text-gray-800 group-hover:text-white">
-                      Github
-                    </span>
-                  </a>
+                <div className="flex flex-col space-y-4 items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-gray-800 rounded-md group hover:bg-gray-300 focus:outline-none">
+                  <span>
+                    <GoogleLogin
+                      onSuccess={responseMessage}
+                      onError={errorMessage}
+                    />
+                  </span>
                 </div>
               </div>
             </form>
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import store from "./Redux/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+      <GoogleOAuthProvider clientId="980495041176-69m85bggaakl1ii66ggfea19l8e7kruu.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
+    </Provider>
   </React.StrictMode>
 );
