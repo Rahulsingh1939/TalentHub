009b37cf91c489a7ed3cde9ad0e76aeead726c28
Rahulsingh1939 <rahulsingh19390@gmail.com>
2024-03-24 13:25:03 +0530

    Added Image Link in Backend

---
+++ backend/controller/uploadController.js
@@ -0,0 +1,46 @@
+const userModel = require("../models/user");
+// Creating new file and text analyze controller
+const createNewFile = async (req, res) => {
+  if (!req.file) {
+    console.error("No image file provided");
+    const error = new Error("No image file provided!");
+    error.statusCode = 422;
+    throw error;
+  }
+  const { userId } = req.body;
+  const imageUrl = req.file.path;
+  console.log("File uploaded at this path: ", imageUrl);
+  const updatedUser = await userModel.findOneAndUpdate(
+    {
+      _id: userId,
+    },
+    { imageUrl: imageUrl },
+    { new: true }
+  );
+
+  if (!updatedUser) {
+    return res.status(401).send({ message: "User not found", success: false });
+  }
+  return res
+    .status(200)
+    .send({ message: "Image Added Successfully", success: true });
+};
+
+// Fetching all created image files controller
+const getFiles = async (req, res, next) => {
+  try {
+    const images = await Images.find();
+    res
+      .status(200)
+      .json({ message: "Fetching all images files successful!", images });
+    console.log("Fetching image files successfull");
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    console.error(err);
+    next(err);
+  }
+};
+
+module.exports = { getFiles, createNewFile };
